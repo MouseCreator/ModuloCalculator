9e47e2beb7088a455a289ad81fce8ac501b317b6
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2025-11-08 19:25:10 +0200

    add wrapper

---
+++ GenAlgAlgorithms/MathBase/Wrapper.cpp
@@ -0,0 +1,45 @@
+
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <stdexcept>
+
+const int MAX_MESSAGE_LENGTH = 4096;
+
+extern "C" char* finite_field(const char* expression, const char* n, char* errorStr)
+{
+    char* resStr = nullptr;
+    resStr = new char[MAX_MESSAGE_LENGTH];
+    std::string s = "0";
+    strcpy(resStr, s.c_str());
+    return resStr;
+}
+
+
+extern "C" char* number_factorization(const char* n, char* errorStr)
+{
+    char* resStr = nullptr;
+    resStr = new char[MAX_MESSAGE_LENGTH];
+    std::string s = "0";
+    strcpy(resStr, s.c_str());
+    return resStr;
+}
+
+extern "C" char* number_order(const char* number, const char* n, char* errorStr)
+{
+    char* resStr = nullptr;
+    resStr = new char[MAX_MESSAGE_LENGTH];
+    std::string s = "0";
+    strcpy(resStr, s.c_str());
+    return resStr;
+}
+
+
+extern "C" char* prime_test(const char* number, const char* iterations, char* errorStr)
+{
+    char* resStr = nullptr;
+    resStr = new char[MAX_MESSAGE_LENGTH];
+    std::string s = "0";
+    strcpy(resStr, s.c_str());
+    return resStr;
+}
