f903d164894260577b02baf694f7a355938ce0ea
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2025-11-19 20:58:47 +0200

    add finite field calculator interface

---
+++ GenAlgAlgorithms/MathBase/AST_FiniteFieldCalculator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <optional>
+
+#include "SignedNumber.h"
+#include "parse/command.h"
+#include "AST_Generic_Visitor.h"
+#include "AST_Context_Type.h"
+#include "FiniteNumber.h"
+namespace MathBase {
+
+	class FiniteFieldCalculator : public ASTVisitor {
+	private:
+		FiniteNumber* _value;
+		FiniteFieldContext context;
+	public:
+		void visitNumber(NumberNode& number) override;
+		void visitVariable(VariableNode& variable) override;
+		void visitOperation(OperationNode& operation) override;
+		void visitUnary(UnaryNode& operation) override;
+		void visitFunction(FunctionNode& func) override;
+
+
+	private:
+	};
+
+}
